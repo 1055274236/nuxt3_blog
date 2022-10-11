eda1d244976c5e1abcca8ec4f7ccb83d17bef01a
Ming <1055274236@qq.com>
2022-10-12 01:27:51 +0800

    移除文件上传/下载模块

--- server/process/index.ts
+++ server/process/index.ts
@@ -2,7 +2,6 @@
  * @Description:
  * @Autor: Ming
  * @LastEditors: Ming
- * @LastEditTime: 2022-10-12 00:19:24
+ * @LastEditTime: 2022-10-12 01:27:02
  */
 export * from './Blog';
-export * from './Download';
