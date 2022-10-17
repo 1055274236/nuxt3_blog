7183c9a455611aec979001d727e16fb08665e1fe
Ming <1055274236@qq.com>
2022-10-18 01:00:58 +0800

    详情页修改

--- pages/details/[id].vue
+++ pages/details/[id].vue
@@ -2,20 +2,62 @@
  * @Description: 
  * @Autor: Ming
  * @LastEditors: Ming
- * @LastEditTime: 2022-10-11 21:11:42
+ * @LastEditTime: 2022-10-18 00:52:36
 -->
 <template>
   <div id="details">
-    <div class="header"></div>
-    <div class="content">
-      <div class="title">
-        {{ data.details.title }}
+    <div class="details-content">
+      <!-- 头部信息 -->
+      <div class="header">
+        <!-- 标题 -->
+        <h1 class="title">
+          {{ data.details.title }}
+        </h1>
+        <!-- 粗略信息 -->
+        <div class="info">
+          <div class="info-content">
+            <!-- 时间 -->
+            <div class="createdAt">
+              <IconClock />
+              {{ dateFormat(data.details.createdAt, 'YYYY-MM-DD') }}
+            </div>
+            <div class="pageview">
+              <IconBrowse />
+              {{ data.details.pageview }}
+            </div>
+            <div class="comment">
+              <IconComment />
+              {{ data.details.comment }}
+            </div>
+            <div class="like">
+              <IconFavorites />
+              {{ data.details.like }}
+            </div>
+            <div class="tag">
+              <IconForm fill />
+              {{ data.details.tag.split(',').join(' ') }}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- 概括 -->
+      <div class="brief">
+        <div class="brief-content">
+          {{ data.details.brief }}
+        </div>
+      </div>
+
+      <!-- 正文 -->
+      <div class="context">
+        <div class="context-content" v-html="data.details.content"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useTitle } from '@vueuse/core';
 import { reactive, onMounted } from 'vue';
 import { BlogRequest } from '~~/api';
 const route = useRoute();
@@ -27,15 +69,11 @@ const data = reactive({
 const result = await blogRequest.getDetails({ id });
 data.details = result.data.details;
 
-// await blogRequest.getDetails({ id }).then((result) => {
-//   data.details = result.data.value.data.details;
-// });
-// console.log(details);
-onMounted(() => {});
+onMounted(() => {
+  useTitle(data.details.title);
+});
 </script>
 
 <style lang="scss" scoped>
-#details {
-  height: 300vh;
-}
+@use './index.scss';
 </style>
